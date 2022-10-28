fcd3265c19a2d9def45f932a1a00ca700312c02c
Adrian Slowik <kontakt@adrianslowik.pl>
2022-10-28 18:49:26 +0200

    recommendations

--- main/tests/bootstrap.php
+++ main/tests/bootstrap.php
@@ -4,6 +4,8 @@ use Tbd\Main\FeatureFlags\EnvOverrider;
 use Tbd\Main\FeatureFlags\FeatureFlag;
 use Tbd\Main\FeatureFlags\InMemoryFeatureFlags;
 
+require __DIR__ . '/../vendor/autoload.php';
+
 $initialFlags = require __DIR__ . '/../src/Flags.php';
 $envOverrider = new EnvOverrider();
 $featureFlags = new InMemoryFeatureFlags($envOverrider->overrideFlags($initialFlags));
